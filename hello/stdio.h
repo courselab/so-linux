0d353c019ff81858564573a41680e9e1c920b4b1
vitoremerique <vitort.emerique@hotmail.com>
2025-05-30 17:58:59 -0300

    Mem kb quebrado

---
+++ hello/stdio.h
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 vitoremerique <vitort.emerique@hotmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  vitoremerique <vitort.emerique@hotmail.com>
+ */
+
+#ifndef E8_H
+#define E8_H
+
+void __attribute__((fastcall, naked)) printf(const char *); 
+ unsigned short memsize(void); 
+
+#endif	
