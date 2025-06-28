754a5930a746008e925bd3e4abf3ffa93465531e
vitoremerique <vitort.emerique@hotmail.com>
2025-06-28 15:12:53 -0300

    mydos

---
+++ mydos/prog.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 vitoremerique <vitort.emerique@hotmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  vitoremerique <vitort.emerique@hotmail.com>
+ */
+
+#include "tydos.h"
+
+int main()
+{
+  puts("Hello World\n");
+  return 0;
+}
